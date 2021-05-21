4d98d009afc777af6a9704fca51ec504932fcd6a
Shanvi725 <78847534+Shanvi725@users.noreply.github.com>
2021-05-21 22:04:46 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		var options = {
+            isStatic:false,
+			'density' :  1.2,
+			'friction': 0,
+			'restitution':0.3
+		  }
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.body = Bodies.circle(this.x, this.y, this.r/2, options);
+        this.image = loadImage("paper.png");
+		World.add(world, this.body);
+	}
+	display(){   
+			var paperpos = this.body.position;		
+			push();
+			translate(paperpos.x, paperpos.y);
+            imageMode(CENTER);
+			image(this.image, 0, 0, this.r, this.r);
+			pop();
+	}
+
+}
